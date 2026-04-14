22febece45ed290fb237c5b7f4365c380f66cdb5
Krystian <154618209+Saido20030@users.noreply.github.com>
2026-04-14 18:08:22 +0200

    Wprowadzanie zmian w Models.

---
+++ SystemRezerwacji/Models/SystemRezerwacji/Services/AppointmentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemRezerwacji.Models;
+
+namespace SystemRezerwacji.Services
+{
+    public class AppointmentService
+    {
+        public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+    }
+}
