3a02e6daccdc715fdbc23afda48b6e24a78313dd
Jakub <habdasjakub@gmail.com>
2026-04-14 17:25:33 +0200

    Przygotowanie nowych modeli.

---
+++ SystemRezerwacji/Models/HistoriaMedyczna.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemRezerwacji.Models
+{
+    public class HistoriaMedyczna
+    {
+        public int IdWpisu { get; set; }
+        public int IdPacjeta { get; set; }
+        public int IdWizyty {  get; set; }
+        public DateTime DataWpisu { get; set; }
+        public string Opis {  get; set; }
+    }
+}
