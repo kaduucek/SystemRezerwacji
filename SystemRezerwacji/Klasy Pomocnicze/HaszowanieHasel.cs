ba304242bac9caf805a9aded27764461f4ebe21a
Krystian <154618209+Saido20030@users.noreply.github.com>
2026-05-05 17:57:01 +0200

    Dodanie logiki hashowania

---
+++ SystemRezerwacji/Klasy Pomocnicze/HaszowanieHasel.cs
@@ -0,0 +1,19 @@
+﻿using BCrypt.Net;
+
+namespace SystemRezerwacji
+{
+    public static class HaszowanieHasel
+    {
+        // Wywołasz to podczas rejestracji pacjenta
+        public static string GenerujHasz(string haslo)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(haslo);
+        }
+
+        // Wywołasz to podczas logowania
+        public static bool WeryfikujHaslo(string wpisaneHaslo, string haszZBase)
+        {
+            return BCrypt.Net.BCrypt.Verify(wpisaneHaslo, haszZBase);
+        }
+    }
+}
