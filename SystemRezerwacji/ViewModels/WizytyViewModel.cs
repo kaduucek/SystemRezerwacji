cf506e9b7d303c6858befcf95c5c6198712ac62a
Jakub <habdasjakub@gmail.com>
2026-05-26 17:23:11 +0200

    Utworzenie ViewModelu dla wyświetlania wizyt.

---
+++ SystemRezerwacji/ViewModels/WizytyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemRezerwacji.Views;
+
+
+namespace SystemRezerwacji.ViewModels
+{
+    public IRelayCommand EdytujWizyteCommand { get; }
+
+    public WizytyViewModel()
+    {
+        EdytujWizyteCommand = new RelayCommand<int>(EdytujWizyte);
+    }
+
+    private void EdytujWizyte(int idWizyty)
+    {
+        var edytujViewModel = new EdytujWizyteViewModel(idWizyty);
+        var okno = new EdytujWizyteView();
+        okno.DataContext = edytujViewModel;
+        okno.ShowDialog();
+
+        LoadWizyty(); 
+    }
+}
