f80574be456c754fd413534183b2ee6fb51ca8d0
Jakub <habdasjakub@gmail.com>
2026-05-26 17:16:28 +0200

    Utworzenie UI dla wyświetlania wizyt.

--- SystemRezerwacji/Views/WizytyView.xaml.cs
+++ SystemRezerwacji/Views/WizytyView.xaml.cs
@@ -24,5 +24,10 @@ namespace SystemRezerwacji.Views
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
