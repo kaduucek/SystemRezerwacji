edbb0a4632c1890fbb55278df71debfcea8b3587
Jakub <habdasjakub@gmail.com>
2026-05-26 17:12:17 +0200

    Dodanie UI do edycji wizyty.

--- SystemRezerwacji/ViewModels/EdytujWizyteViewModel.cs
+++ SystemRezerwacji/ViewModels/EdytujWizyteViewModel.cs
@@ -5,7 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows; 
+using System.Windows;
+using SystemRezerwacji.Models;
 
 namespace SystemRezerwacji.ViewModels
 {
